96ff18764ee11537e9918818dc18e4389fcaae45
stefanerefrande <stefanerefrande@gmail.com>
2022-04-03 00:47:53 -0300

    Adds views

--- LocalMarket/LocalMarket/LocalMarket/urls.py
+++ LocalMarket/LocalMarket/LocalMarket/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.ApiOverview, name='home')
+  path('', views.ApiOverview, name='home'),
+  path('create/', views.add_items, name='add_items'),
+  path('all/', views.view_items, name='view_items'),
+  path('update/<int:pk>/', views.update_items, name='update_items'),
+  path('item/<int:pk>/delete/', views.delete_items, name='delete_items'),
 ]
--- LocalMarket/LocalMarket/LocalMarket/views.py
+++ LocalMarket/LocalMarket/LocalMarket/views.py
@@ -3,6 +3,9 @@ from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
 
+from LocalMarket.LocalMarket.LocalMarket.models import Item
+from LocalMarket.LocalMarket.LocalMarket.serializer import ItemSerializer
+
 
 @api_view(['GET'])
 def ApiOverview(request):
@@ -17,4 +20,50 @@ def ApiOverview(request):
 
     return Response(api_urls)
 
+
+@api_view(['POST'])
+def add_items(request):
+    item = ItemSerializer(data=request.data)
+
+    if Item.objects.filter(**request.data).exists():
+        raise serializers.ValidationError('This item already exists')
+
+    if item.is_valid():
+        item.save()
+        return Response(item.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET'])
+def view_items(request):
+    if request.query_params:
+        items = Item.objects.filter(**request.query_param.dict())
+    else:
+        items = Item.objects.all()
+
+    if items:
+        data = ItemSerializer(items)
+        return Response(data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['POST'])
+def update_items(request, pk):
+    item = Item.objects.get(pk=pk)
+    data = ItemSerializer(instance=item, data=request.data)
+
+    if data.is_valid():
+        data.save()
+        return Response(data.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['DELETE'])
+def delete_items(request, pk):
+    item = Item.objects.get(pk=pk)
+    item.delete()
+    return Response(status=status.HTTP_202_ACCEPTED)
 
